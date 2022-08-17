357afa42557b1d3fbc1ec1a12cdf502eb6bf6865
Oscar Monsreal <A15216352@alumnos.uady.mx>
2022-08-16 19:14:03 -0500

    [feature] Creacion de funciones para el uso de guards (admins, autorizadas)

--- app/Http/Controllers/LoginController.php
+++ app/Http/Controllers/LoginController.php
@@ -4,26 +4,79 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
-use Illuminate\Support\Facades\Hash;
-use App\Models\User;
+use App\Models\Administrador;
+use App\Models\PersonasAutorizadas;
 
 class LoginController extends Controller
 {
-
-    public function acceso(){
-        return view('auth.login');
+    public function __construct()
+    {
+        $this->middleware('guest')->except('logout');
+        $this->middleware('guest:admin')->except('logout');
+        $this->middleware('guest:autorizados')->except('logout');
     }
 
-    public function login(Request $request){
-        $credentials = $request->only('email', 'password');
-        if(Auth::attempt($credentials)){
-            return redirect()->route('home');
-        }else{
-           dd('No esta autorizado');
+    /**
+     * Seccion ADMINISTRADOR
+     */
+    public function vistaLoginAdmin()
+    {
+        return view('admin.auth.login');
+    }
+    public function loginAdministrador(Request $request)
+    {
+        $credenciales = $request->validate([
+            'email' => 'required|email',
+            'password' => 'required|min:6',
+        ]);
+        $administrador = Administrador::where('email', $request->email)->first();
+
+        if(!is_null($administrador)){
+            if (Auth::guard('admin')->attempt($credenciales)) {
+                $request->session()->regenerate();
+                return redirect()->route('');
+            }
         }
     }
 
-    public function logout(Request $request){
+    /**
+     * Seccion PERSONAS AUTORIZADAS
+     */
+    public function vistaPersonaAutorizada(){
+        return view('autorizados.auth.login');
+    }
+    public function loginPersonaAutorizada(Request $request){
+        $credenciales = $request->validate([
+            'email' => 'required|email',
+            'password' => 'required|min:6',
+        ]);
+        $personaAutorizada = PersonasAutorizadas::where('email', $request->email)->first();
+        if(!is_null($personaAutorizada)){
+            if(Auth::guard('autorizados')->attempt($credenciales)){
+                $request->session()->regenerate();
+                return redirect()->route('');
+            }
+        }
+    }
+
+
+    public function acceso()
+    {
+        return view('auth.login');
+    }
+
+    public function login(Request $request)
+    {
+        $credentials = $request->only('email', 'password');
+        if (Auth::attempt($credentials)) {
+            return redirect()->route('home');
+        } else {
+            dd('No esta autorizado');
+        }
+    }
+
+    public function logout(Request $request)
+    {
         Auth::logout();
 
         $request->session()->invalidate();
