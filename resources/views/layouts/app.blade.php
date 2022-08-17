a7dfa55d54aa4ddac98cf653907318a1d757cf35
Oscar Monsreal <A15216352@alumnos.uady.mx>
2022-08-16 19:17:59 -0500

    [hotfix] Modificacion de layouts

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,5 +1,6 @@
 <!doctype html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
@@ -18,52 +19,59 @@
 
     <!-- Styles -->
     {{-- <link href="{{ asset('css/app.css') }}" rel="stylesheet"> --}}
-    
+
     <!--   Core JS Files   -->
-    <script src="https://cdn.jsdelivr.net/npm/jquery/dist/jquery.min.js"></script> 
+    <script src="https://cdn.jsdelivr.net/npm/jquery/dist/jquery.min.js"></script>
     <link rel="stylesheet" href="https://unpkg.com/bootstrap-table@1.20.2/dist/bootstrap-table.min.css">
 
     <script src="//cdnjs.cloudflare.com/ajax/libs/bootstrap-table/1.20.1/bootstrap-table.min.js"></script>
-{{--     <script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-select/1.14.0-beta3/js/bootstrap-select.min.js"></script>
- --}}    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-select/1.14.0-beta3/css/bootstrap-select.min.css">
-  <script src="{{ asset('js/core/popper.min.js') }}"></script>
-  <script src="{{ asset('js/plugins/perfect-scrollbar.min.js') }}"></script>
-  <script src="{{ asset('js/plugins/smooth-scrollbar.min.js') }}"></script>
-  {{-- Ajax and JQuery --}}
+    {{-- <script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-select/1.14.0-beta3/js/bootstrap-select.min.js"></script> --}}
+    <link rel="stylesheet"
+        href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-select/1.14.0-beta3/css/bootstrap-select.min.css">
+    <script src="{{ asset('js/core/popper.min.js') }}"></script>
+    <script src="{{ asset('js/plugins/perfect-scrollbar.min.js') }}"></script>
+    <script src="{{ asset('js/plugins/smooth-scrollbar.min.js') }}"></script>
+    {{-- Ajax and JQuery --}}
 
 
-  <meta charset="utf-8" />
-  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-  <link rel="apple-touch-icon" sizes="76x76" href="../../../assets/img/apple-icon.png">
-  <link rel="icon" type="image/png" href="../../../assets/img/favicon.png">
-  <title>
-    CirceNot
-  </title>
-  <!--     Fonts and icons     -->
-  <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,600,700" rel="stylesheet" />
-  <!-- Nucleo Icons -->
-  <link href="{{ asset('css/nucleo-icons.css') }}" rel="stylesheet" />
-  <link href="{{ asset('css/nucleo-svg.css') }}" rel="stylesheet" />
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <link rel="apple-touch-icon" sizes="76x76" href="../../../assets/img/apple-icon.png">
+    <link rel="icon" type="image/png" href="../../../assets/img/favicon.png">
+    <title>
+        CirceNot
+    </title>
+    <!--     Fonts and icons     -->
+    <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,600,700" rel="stylesheet" />
+    <!-- Nucleo Icons -->
+    <link href="{{ asset('css/nucleo-icons.css') }}" rel="stylesheet" />
+    <link href="{{ asset('css/nucleo-svg.css') }}" rel="stylesheet" />
 
 
-  <!-- Font Awesome Icons -->
-  <script src="https://kit.fontawesome.com/42d5adcbca.js" crossorigin="anonymous"></script>
-  <link href="{{ asset('css/nucleo-svg.css') }}" rel="stylesheet" />
+    <!-- Font Awesome Icons -->
+    <script src="https://kit.fontawesome.com/42d5adcbca.js" crossorigin="anonymous"></script>
+    <link href="{{ asset('css/nucleo-svg.css') }}" rel="stylesheet" />
 
-  <!-- CSS Files -->
-  <link href="{{ asset('css/argon-dashboard.css?v=2.0.5') }}" rel="stylesheet" />
+    <!-- CSS Files -->
+    <link href="{{ asset('css/argon-dashboard.css?v=2.0.5') }}" rel="stylesheet" />
 
-  <!-- Kanban scripts -->
-  <script src="{{ asset('js/plugins/dragula/dragula.min.js') }}"></script>
-  <script src="{{ asset('js/plugins/jkanban/jkanban.js') }}"></script>
-  <script src="{{ asset('js/plugins/chartjs.min.js') }}"></script>
+    <!-- Kanban scripts -->
+    <script src="{{ asset('js/plugins/dragula/dragula.min.js') }}"></script>
+    <script src="{{ asset('js/plugins/jkanban/jkanban.js') }}"></script>
+    <script src="{{ asset('js/plugins/chartjs.min.js') }}"></script>
 
-   <!--  Plugin for TiltJS, full documentation here: https://gijsroge.github.io/tilt.js/ -->
-   <script src="{{ asset('js/plugins/tilt.min.js') }}"></script>
-   <script src="//unpkg.com/bootstrap-table@1.14.2/dist/bootstrap-table.min.js"></script>
+    <!--  Plugin for TiltJS, full documentation here: https://gijsroge.github.io/tilt.js/ -->
+    <script src="{{ asset('js/plugins/tilt.min.js') }}"></script>
+    <script src="//unpkg.com/bootstrap-table@1.14.2/dist/bootstrap-table.min.js"></script>
 
 </head>
-<body>
 
+<body>
+    @include('layouts.navBar')
+    <main>
+        @yield('content')
+    </main>
+    @yield('scripts')
 </body>
+
 </html>
--- resources/views/layouts/navBar.blade.php
+++ resources/views/layouts/navBar.blade.php
@@ -1,4 +1,3 @@
-@extends('layouts.app')
 <nav class="navbar navbar-main navbar-expand-lg  px-0 mx-4 shadow-none border-radius-xl z-index-sticky " id="navbarBlur" data-scroll="false">
     <div class="container-fluid py-1 px-3">
       <nav aria-label="breadcrumb">
@@ -127,4 +126,3 @@
       </div>
     </div>
   </nav>
-  
