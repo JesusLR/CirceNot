a23e20517dab736648a27211b5239c9ac3eda849
oscar.monsreal <oscar.monsreal@insejupy.gob.mx>
2022-11-24 08:38:37 -0600

    [Update] Creacion de migraciones faltantes

--- database/migrations/2022_09_29_011625_create_clientes_table.php
+++ database/migrations/2022_09_29_011625_create_clientes_table.php
@@ -13,8 +13,40 @@ class CreateClientesTable extends Migration
      */
     public function up()
     {
-        Schema::create('clientes', function (Blueprint $table) {
-            $table->id();
+        Schema::create('tbClienteF', function (Blueprint $table) {
+            $table->id('idClient');
+            $table->string('cNombre');
+            $table->string('cApellidoMat');
+            $table->string('cApellidoPat');
+            $table->string('cEmail');
+            $table->string('iTel');
+            $table->string('iCel');
+            $table->string('cCURP');
+            $table->string('cRFC');
+            $table->string('cOcupacion');
+            $table->string('cPaisNacimiento');
+            $table->string('cEstadoCivil');
+            $table->string('cNombreConyugue');
+            $table->string('iCalle1');
+            $table->string('iNumExt1');
+            $table->string('iNumInt1');
+            $table->string('cCodPost1');
+            $table->string('cColonia1');
+            $table->string('cCiudad1');
+            $table->string('cEstado1');
+            $table->string('iCalle2');
+            $table->string('iNumExt2');
+            $table->string('iNumInt2');
+            $table->string('cCodPost2');
+            $table->string('cColonia2');
+            $table->string('cCiudad2');
+            $table->string('cEstado2');
+            $table->string('cIdentificacionRuta');
+            $table->string('cActaRuta');
+            $table->string('cComprobanteRuta');
+            $table->string('cFechaVencimientoRuta');
+            $table->string('iTipo');
+            $table->boolean('lActivo');
             $table->timestamps();
         });
     }
@@ -26,6 +58,6 @@ class CreateClientesTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('clientes');
+        Schema::dropIfExists('tbClienteF');
     }
 }
---
+++ database/migrations/2022_11_24_142850_create_tbclientesm_table.php
@@ -0,0 +1,45 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTbclientesmTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('tbclientesM', function (Blueprint $table) {
+            $table->id();
+            $table->string('cRazonSocial');
+            $table->string('cEmail');
+            $table->string('iTel');
+            $table->string('iCel');
+            $table->string('cDomicilioFiscal');
+            $table->string('cEntidad');
+            $table->string('cCodigoPost');
+            $table->string('cRFC');
+            $table->string('cRegimenFisc');
+            $table->string('iIDClienteF');
+            $table->string('cActaRuta');
+            $table->string('cDocumentacionRuta');
+            $table->string('iTipo');
+            $table->boolean('lActivo');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('tbclientesM');
+    }
+}
